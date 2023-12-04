2f3ee84ab9db0a9efc53a47adbb3cbf115cfa2a3
oyasso <devochtyasson1@gmail.com>
2023-12-04 16:15:56 +0100

    2_IdentiekeFilenameInhoud

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 from jinja2 import Environment, FileSystemLoader
+from pathlib import Path
 
-auteurs = ["student_0", "student_1", "student_2"]  # lijst met anonieme studenten
+dir = Path("alle_auteurs")
+
+auteurs = {subdir.stem: {file.stem: file.read_text()} for subdir in dir.iterdir() for file in subdir.iterdir()}
 auteurs_prive = {"Barbius": "student_0", "Justilius": "student_1", "Cervus": "student_2"}  # dict met echte namen
 
-combos = {auteurs[i]: {auteurs[j]: [] for j in range(3)} for i in range(3)}  # dict comp
+combos = {auteur: {student: "" for student in auteurs} for auteur in auteurs}  # dict comp
 # dict om te renderen
 context = {
     "studenten": combos
 }
 
-# manuele toevoeging van plagiaat_vormen
-combos["student_0"]["student_0"] = "/"
-combos["student_0"]["student_1"] = ["zelfde alinea"]
-combos["student_0"]["student_2"] = ["Plagiaatvrij"]
-combos["student_1"]["student_0"] = ["zelfde alinea"]
-combos["student_1"]["student_1"] = "/"
-combos["student_1"]["student_2"] = ["zelfde bronnen", "zelfde titel"]
-combos["student_2"]["student_0"] = ["Plagiaatvrij"]
-combos["student_2"]["student_1"] = ["zelfde bronnen", "zelfde titel"]
-combos["student_2"]["student_2"] = "/"
+# plagiaat schrijven als file hetzelde is
+for x in combos:
+    for y in combos[x]:
+        if combos[x][y] == combos[x]:
+            combos[x][y] = "/"
+        else:
+            if auteurs[x] == auteurs[y]:
+                combos[x][y] = "zelfde file"
+            else:
+                combos[x][y] = " "
+
 
 # jinja setup
 environment = Environment(loader=FileSystemLoader("templates/"))
 output_template = environment.get_template("outputtemplate.html")
 # write to file
-with open("templates/outputtemplate.html", mode="w", encoding="utf-8") as o:
+with open("output.html", mode="w", encoding="utf-8") as o:
     o.write(output_template.render(context))
--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-studenten = {
-    "student_0": {
-        "student_0": "/",
-        "student_1": " "
-    },
-    "student_1": {
-        "student_0": " ",
-        "student_1": "/"
-    }
-}
+from pathlib import Path
+import json
 
-for student in studenten:
-    print(studenten.get(student).get("student_1", "Not Found"))
+dir = Path("alle_auteurs")
+
+for subdir in dir.iterdir():
+    for file in subdir.iterdir():
+        if file.is_file():
+            print(file.stem)
+
+auteurs = [subdir.stem for subdir in dir.iterdir()]
+auteurs2 = {subdir.stem: {file.stem: file.read_text()} for subdir in dir.iterdir() for file in subdir.iterdir()}
+combos = {auteur: {student: [] for student in auteurs} for auteur in auteurs}  # dict comp
+
+print(auteurs2)
+print(json.dumps(combos, indent=4))
